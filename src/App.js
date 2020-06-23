3211a55b649b4221e1b40c66be0ad98bd6bff125
Sharon Cheung <keikei1128@g.ucla.edu>
2020-06-22 22:43:02 -0700

    changed from class componet to function

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import PropTypes from 'prop-types';
 import axios from 'axios';
@@ -17,60 +17,58 @@ import Customers from './components/Customers';
 import CustomerDetail from './components/CustomerDetail';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-class App extends Component {
-  state = {
-    movies: []
-  }
+const App = () => {
 
-  componentDidMount() {
-    axios.get(this.props.url+'movies')
-      .then(res => {
-        const movies = res.data;
-        console.log(movies);
-        this.setState({ movies });
-      })
-  }
+  const base_url = "http://localhost:3000"
 
-  render() {
-    return (
-      <Router>
-      <div>
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <ul>
-          <li className="nav-item active">
-            <Link to="/">Home</Link>
-          </li>
-          <li class="nav-item active">
-            <Link to="/search">Search</Link>
-          </li>
-          <li class="nav-item active">
-            <Link to="/library">Library</Link>
-          </li>
-          <li class="nav-item active">
-            <Link to="/customers">Customers</Link>
-          </li>
-          <li class="nav-item active">
-            <Link to="/customerdetail">Customer Detail</Link>
-          </li>
-        </ul>
+  return (
+    <Router>
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <ul>
+        <li className="nav-item active">
+          <Link to="/">Home</Link>
+        </li>
+        <li class="nav-item active">
+          <Link to="/search">Search</Link>
+        </li>
+        <li class="nav-item active">
+          <Link to="/library">Library</Link>
+        </li>
+        <li class="nav-item active">
+          <Link to="/customers">Customers</Link>
+        </li>
+        <li class="nav-item active">
+          <Link to="/customerdetail">Customer Detail</Link>
+        </li>
+      </ul>
+      </nav>
+      <div className="">
+        <nav className="">
+        <Switch>
+          <Route path="/">
+            <Home />
+            </Route>
+          <Route path="/search">
+            <Search url={base_url}/> 
+          </Route>
+          <Route path="/library">
+            <Library url={base_url}/>
+          </Route> 
+          <Route path='/customers'>
+            <Customers url={base_url}/>
+          </Route> 
+          <Route path="/customerdetail">
+            <CustomerDetail url={base_url}/>
+          </Route> 
+        </Switch>
         </nav>
-        <div className="">
-          <nav className="">
-          <Switch>
-            <Route path="/" exact component={Home} />
-            <Route path="/search" component={Search} />
-            <Route path="/library" component={Library} />
-            <Route path="/customers" component={Customers} />
-            <Route path="/customerdetail" component={CustomerDetail} />
-          </Switch>
-          </nav>
-          </div>
-      </div>
-      </Router>
-    
-    );
-  }
+        </div>
+    </div>
+    </Router>
+  );
 }
+
     App.propTypes = {
       url: PropTypes.string.isRequired,
     }
--- src/components/Library.js
+++ src/components/Library.js
@@ -1,7 +1,25 @@
-import React from 'react';
+// import React from 'react';
 import './Library.css';
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import axios from 'axios';
 
-const Library = () => {
+const Library = ({url}) => {
+  const [ movieList, setMovieList ] = useState([]);
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  useEffect(() => {
+    axios.get(url+'movies')
+      .then((response) => {
+        const apiMovieList = response.data;
+        console.log(apiMovieList);
+        setMovieList(apiMovieList);
+      })
+      .catch((error) => {
+        setErrorMessage(error.message);
+      });
+  },[]);
+  
   return (
     <div className="">
       <h2>Library</h2>
