0f0922346d59be921cd80fee3e17845ffc577acc
Sharon Cheung <keikei1128@g.ucla.edu>
2020-06-22 18:28:44 -0700

    added some Boostrap nav bar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import PropTypes from 'prop-types';
+import axios from 'axios';
 
 import {
   BrowserRouter as Router,
@@ -15,29 +15,32 @@ import Search from './components/Search';
 import Library from './components/Library';
 import Customers from './components/Customers';
 import CustomerDetail from './components/CustomerDetail';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
   render() {
     return (
       <Router>
       <div>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <ul>
-          <li>
+          <li class="nav-item active">
             <Link to="/">Home</Link>
           </li>
-          <li>
+          <li class="nav-item active">
             <Link to="/search">Search</Link>
           </li>
-          <li>
+          <li class="nav-item active">
             <Link to="/library">Library</Link>
           </li>
-          <li>
+          <li class="nav-item active">
             <Link to="/customers">Customers</Link>
           </li>
-          <li>
+          <li class="nav-item active">
             <Link to="/customerdetail">Customer Detail</Link>
           </li>
         </ul>
+        </nav>
         <div className="">
           <nav className="">
             <Route path="/" exact component={Home} />
