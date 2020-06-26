7bb82819ab6b8ba240f91dec8ee6dbaceb144dda
Emily Cowan <e.anderson8977@gmail.com>
2020-06-26 07:05:15 -0700

    Added sharon's JSX and component code

--- src/components/Search.js
+++ src/components/Search.js
@@ -9,25 +9,24 @@ const Search = ({baseUrl}) => {
   const [query, setQuery] = useState('');
   const [movies, setMovies] = useState([]);
   const [errorMessage, setErrorMessage] = useState(null);
+  const [libraryMovieList, setLibraryMovieList] = useState([]);
+  const [flash, setFlash] = useState("");
 
-  const setErrorMessage = () => {
-
-  }
-
-  const setMessage = () => {
-
-  }
+  useEffect(() => {
+    updateMovie();
+  },[]);
 
   const handleChange = (event) => {
 
     event.preventDefault();
 
-    if (query) {
+    if (query && query !== "") {
+
       setQuery(event.target.value);
+
       axios.get(baseUrl+'movies?query=<'+query+'>')
         .then((response) => {
           const apiMoviesList = response.data;
-          console.log(apiMoviesList);
           setMovies(apiMoviesList);
         })
         .catch((error) => {
@@ -46,21 +45,20 @@ const Search = ({baseUrl}) => {
     })
   }
 
-  const SearchComponent = movies.map((movie, i) => {
+  const libraryResultComponent = apiMoviesList.map((movie, i) => {
     return (
       <Movie
         key={movie.external_id}
         {...movie}
-        // TODO: add onClickCallBack for selecting the movie
-        // movieClickCallback={movieClickCallback}
-        // action={"Select Movie"} 
+        onClickCallBack={onClickCallBack} 
+        action={"Select Movie"}
       />
     )
   });
 
   return (
     <div className="container">
-      <h3>Search A Movie</h3>
+      <h3>Search For a Movie</h3>
       <form className="form-inline" onSubmit={handleChange}>
         <input  className="form-control mr-sm-2" type="search" 
                 placeholder="Search for a movie.." aria-label="Search" 
@@ -68,9 +66,26 @@ const Search = ({baseUrl}) => {
         />
         <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
       </form>
-      {SearchComponent}
-    </div>
-    
+      { flash ? <p className="center-error-message alert alert-success">{ flash }</p> : '' }
+      { errorMessage ? <p className="center-error-message alert alert-danger">{ errorMessage }</p> : '' }
+      {libraryMovieList.length >= 1 ? 
+        <div>
+        <h3>Library Result</h3>
+        <div className="movie_list">
+          {libraryResultComponent}
+        </div> 
+        </div>: ""
+      }
+      {movies.length >= 1 ? 
+        <div>
+          <h3>External Result</h3>
+          <h6>Add a movie that you love!</h6>
+          <div className="movie_list">
+            {SearchComponent}
+          </div>
+        </div> : ""
+      }
+      </div> 
   )
 }
 
